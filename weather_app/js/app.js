779f5e9ab09704ad5b5a21f010a55bb55b1e3e14
Abdellahsyani <syaniabdos@gmail.com>
2026-02-22 12:44:19 +0000

    add events

--- weather_app/js/app.js
+++ weather_app/js/app.js
@@ -3,3 +3,16 @@ const v = document.querySelector(".temp");
 x.addEventListener("click", function () {
   v.classList.toggle("show");
 });
+
+const cel = document.querySelector(".cel");
+const fah = document.querySelector(".fah");
+
+cel.addEventListener("click", () => {
+  cel.classList.add("active");
+  fah.classList.remove("active");
+});
+
+fah.addEventListener("click", () => {
+  fah.classList.add("active");
+  cel.classList.remove("active");
+});
