948a0e595886124eb827c1e5c47446525bd6b136
Abdellahsyani <syaniabdos@gmail.com>
2026-03-01 13:28:02 +0000

    divs handler

--- weather_app/js/app.js
+++ weather_app/js/app.js
@@ -6,6 +6,10 @@ x.addEventListener("click", function () {
 
 const cel = document.querySelector(".cel");
 const fah = document.querySelector(".fah");
+const km = document.querySelector(".km");
+const mp = document.querySelector(".mp");
+const mili = document.querySelector(".mili");
+const inches = document.querySelector(".inches");
 
 cel.addEventListener("click", () => {
   cel.classList.add("active");
@@ -15,4 +19,24 @@ cel.addEventListener("click", () => {
 fah.addEventListener("click", () => {
   fah.classList.add("active");
   cel.classList.remove("active");
+});
+
+km.addEventListener("click", () => {
+  km.classList.add("active");
+  mp.classList.remove("active");
+});
+
+mp.addEventListener("click", () => {
+  mp.classList.add("active");
+  km.classList.remove("active");
+});
+
+mili.addEventListener("click", () => {
+  mili.classList.add("active");
+  inches.classList.remove("active");
+});
+
+inches.addEventListener("click", () => {
+  inches.classList.add("active");
+  mili.classList.remove("active");
 });
